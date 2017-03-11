cd507fa8c7f8466206df970f72e6b216840ba18d
bb-blud <rsalo001@gmail.com>
2017-03-11 14:43:25 -0500

    feat: Finish ball logic and its kv features
    Wrapped x and y velocities in *velocity* variable using ReferenceLisProperty
    imported from kivy.properties. In this way using *velocity* to construct the
    2d velocity vector neatly. Also PongBall's move function is now a one liner.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.vector import Vector
 
 class PongGame(Widget):
     pass
@@ -11,7 +13,13 @@ class PongBall(Widget):
 
     # referencelist property so we can use ball.velocity as
     # a shorthand, just like e.g. w.pos for w.x and w.y
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
 
+    # ``move`` function will move the ball one step. This
+    #  will be called in equal intervals to animate the ball
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self
     
 class PongApp(App):
     def build(self):
